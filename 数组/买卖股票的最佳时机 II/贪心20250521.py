9a6ec4ef4cfe7d33efe8d19dace7ef9b8243f650
althealam <althealam0531@gmail.com>
2025-05-21 19:56:33 +0800

    commit

---
+++ 数组/买卖股票的最佳时机 II/贪心20250521.py
@@ -0,0 +1,9 @@
+# 时间复杂度：O(n)
+# 空间复杂度：O(1)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res=[0]*(len(prices)-1)
+        for i in range(len(prices)-1):
+            res[i]=prices[i+1]-prices[i]
+        return sum([r for r in res if r>0])
+        
